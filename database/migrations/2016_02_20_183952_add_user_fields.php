4f5d8c8b564592d4ee8e1a74dd46ac203202bbb0
Andreas Ek <andreas@andreasek.se>
2016-02-20 19:59:16 +0100

    Migreringsfält till databasen

---
+++ database/migrations/2016_02_20_183952_add_user_fields.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserFields extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->string('member_id');
+            $table->string('mobile');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
