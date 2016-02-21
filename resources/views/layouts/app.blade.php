96c5f805c4058fd94d3727e80aa3e1d52085bd9c
Andreas Ek <andreas@andreasek.se>
2016-02-21 21:45:00 +0100

    Remove home url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,7 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/hem') }}">Hem</a></li>
+                    <li><a href="{{ url('/') }}">Hem</a></li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
